17758918327048210e09c919ea9ebc1d1fbc2abb
Jeann314 <jeanncbm@gmail.com>
2025-11-17 16:36:27 -0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title = "Vacinações",layout="wide")
+
+st.title("DASHBOARD VACINAÇÃO")
+
+df = pd.read_csv('vacinacao_corrigido.csv')
+
+fig1 = px.line(df, x = 'date', y = 'total_vaccinations', color = 'location',
+               title = 'Total de vacinações por país')
+fig1.update_layout(xaxis_title = 'Data', yaxis_title = 'Total de vacinações', title_font_size = 30)
+fig1.show()
+
+st.plotly_chart(fig1, use_container_width = True)
+
+df_brasil_usa_india = df.query('location == "Brazil" or location == "United States of America" or location == "India"')
+fig2 = px.pie(df_brasil_usa_india, values = 'total_vaccinations', names = 'location', title = 'Total de vacinados no Brasil, USA e Índia')
+fig2.show()
+
+st.plotly_chart(fig2, use_container_width = True)
 
